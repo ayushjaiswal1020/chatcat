dac7ffd0d1fc484e4e1860b8af1d64154d7d2878
Ayush Jaiswal <69688537+ayushjaiswal1020@users.noreply.github.com>
2021-05-19 12:50:17 +0530

    Update start.py

--- start.py
+++ start.py
@@ -2,28 +2,44 @@ from os import environ
 import aiohttp
 from pyrogram import Client, filters
 
-
-from datetime import datetime
-
-
-from telethon import Button, events
-from telethon.utils import get_display_name
-
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
+API_KEY = environ.get('API_KEY', 'c3398d623ffc9ab6de497e37d6938e280ed7a474')
 
-bot = Client('Chatcat bot',
+bot = Client('gplink bot',
              api_id=API_ID,
              api_hash=API_HASH,
              bot_token=BOT_TOKEN,
              workers=50,
              sleep_threshold=10)
 
+
 @bot.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
-await message.reply(
-              f"**Hey there {message.chat.first_name}, this is Assistant of MJBhai!**\n\n"
-              "You can contact my boss using this bot!! \n\n Send your Message, I will Deliver it to my Boss.")
+    await message.reply(
+        f"**𝗛𝗘𝗟𝗟𝗢🎈{message.chat.first_name}!**\n\n"
+        "𝗜'𝗺 𝗹𝗶𝗻𝗸 Shortener 𝗯𝗼𝘁. 𝗝𝘂𝘀𝘁 𝘀𝗲𝗻𝗱 𝗺𝗲 𝗹𝗶𝗻𝗸 𝗮𝗻𝗱 𝗴𝗲𝘁 𝗦𝗵𝗼𝗿𝘁𝗲𝗻𝗲𝗱 𝗨𝗥𝗟. \n\n 𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗜𝘀 𝗠𝗮𝗱𝗲 𝗕𝘆 @infozoxdesign💖")
+
+
+@bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
+async def link_handler(bot, message):
+    link = message.matches[0].group(0)
+    try:
+        short_link = await get_shortlink(link)
+        await message.reply(f'Here is your👉 [Short Link🎈]({short_link})', quote=True)
+    except Exception as e:
+        await message.reply(f'Error: {e}', quote=True)
+
+
+async def get_shortlink(link):
+    url = 'https://earnspaces.com/api'
+    params = {'api': API_KEY, 'url': link}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True) as response:
+            data = await response.json()
+            return data["shortenedUrl"]
+
 
 bot.run()
